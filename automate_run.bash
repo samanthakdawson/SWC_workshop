4c261e9ac6f5c418114821925040cd85453b6e48
samanthakdawson <samantha.k.dawson@gmail.com>
2018-10-11 15:34:00 +0200

    updated comments

--- automate_run.bash
+++ automate_run.bash
@@ -5,9 +5,13 @@ RESULTS_DIR=/Users/samanthadawson/Documents/data-shell/results
 
 #do not change from here unless you know what you are doing!
 
+
+#first remove previous versions so as not to concatanate into an existing file
 rm -rf $RESULTS_DIR/
 mkdir $RESULTS_DIR 
 
+
+#then gets the number of line counts in each file, sorts them, and pulls out the smallest
 for filename in $INPUT_DIR/*.pdb 
  do
   wc -l $filename | sort -n | head -n 1 >> $RESULTS_DIR/lengths.txt 
